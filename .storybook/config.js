58ad06b7811eb7e8985f3660dfb353d0ad6f23b4
d4n13ly <kiwidan92@gmail.com>
2018-08-15 16:37:28 +1200

    Add example button component

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,14 @@
-import { configure } from '@storybook/vue';
+import { configure, storiesOf } from '@storybook/vue';
 import { registerStories } from 'vue-storybook';
 import Vue from 'vue';
+
+import '../styles/main.scss';
 
 const req = require.context('../components', true, /\.vue$/);
 
 function loadStories() {
   req.keys().forEach(filename => {
-    registerStories(req, filename, storiesOf);
+    registerStories(req, filename, storiesOf, {});
   });
 }
 
---
+++ components/Button.vue
@@ -0,0 +1,45 @@
+<template>
+  <a class="button">
+    <slot></slot> 
+  </a>
+</template>
+
+<script>
+  export default {
+    name: 'Button',
+  }
+</script>
+
+<style lang="scss">
+  @import '../styles/lib/manifest';
+
+  @mixin swapColours($backgroundColour, $textColour) {
+    background: $backgroundColour;
+    border-color: $backgroundColour;
+    color: $textColour;
+    &:hover {
+      background: $textColour;
+      color: $backgroundColour;
+    }
+  }
+
+  .button {
+    display: inline-block;
+    border: 2px solid; 
+    height: 24px;
+    line-height: 24px;
+    padding: 12px;
+    border-radius: 3px;
+    @include swapColours($brand-primary, $white);
+    &.secondary {
+      @include swapColours($brand-secondary, $white);
+    }
+  }
+</style>
+
+<story name="Buttons">
+  <div>
+    <Button>Action Button</Button>
+    <Button class="secondary">Secondary Button</Button>
+  </div>
+</story>
